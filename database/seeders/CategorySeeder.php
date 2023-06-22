e0d2ff097089648b49af48751eae08e65ee38658
gluke0 <milanesigianluca@gmail.com>
2023-06-22 16:34:08 +0200

    Category Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +17,14 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ['Tech', 'Food & Beverage', 'Travel', 'Miscellaneous', 'Education', 'Entertainment' ];
+
+        foreach($categories as $elem){
+            $new_category = new Category();
+            $new_category->name = $elem;
+            $new_category->slug = Str::slug($new_category->name, '-');
+            $new_category->save();
+        }
+        
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Symfony\Component\Translation\MessageCatalogueInterface;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,7 +23,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            ProjectSeeder::class
+            ProjectSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
